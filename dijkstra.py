69302e566afda069942ed07f98e42f458d52f106
koji-sauna <koji.1127.403@outlook.jp>
2021-02-27 13:54:12 +0900

    initial commit

---
+++ dijkstra.py
@@ -0,0 +1,23 @@
+from heapq import heappush, heappop
+
+# ノード数, エッジ数, 始点ノード
+v, e, r = map(int, input().split())
+node = [[] for _ in range(v+1)]
+for i in range(e):
+    s, t, d = map(int, input().split())
+    node[s].append((t, d))
+    
+INF = 10 ** 9
+def dijkstra(s, n): # start and number of nodes
+    dist = [INF] * n
+    hq = [(0, s)] # (cost, node)
+    dist[s] = 0
+    checked = [False] * n 
+    while hq:
+        v = heappop(hq)[1] # get a node
+        checked[v] = True
+        for to, cost in node[v]: 
+            if checked[to] == False and dist[v] + cost < dist[to]:
+                dist[to] = dist[v] + cost
+                heappush(hq, (dist[to], to))
+    return dist
