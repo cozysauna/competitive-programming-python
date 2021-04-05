2eb95ac57e8cb7772546e277b3198811bb1954bc
Cozy <75005766+cozysauna@users.noreply.github.com>
2021-04-05 11:34:44 +0900

    Update nCr.py

--- nCr.py
+++ nCr.py
@@ -6,9 +6,3 @@ def combinations_count(n, r):
     numer = reduce(mul, range(n, n - r, -1), 1)
     denom = reduce(mul, range(1, r + 1), 1)
     return numer // denom
-
-print(combinations_count(4, 2))
-# 6
-
-print(combinations_count(4, 0))
-# 1
