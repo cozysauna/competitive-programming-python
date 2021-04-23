0426e88a1eb97c2e5bddcd3793ac55d9d68ea10f
塩見晃司 <koji@cozy-sauna.local>
2021-04-23 15:37:07 +0900

    modified nCr.py

--- nCr.py
+++ nCr.py
@@ -1,7 +1,7 @@
 from operator import mul
 from functools import reduce
 
-def combinations_count(n, r):
+def nCr(n, r):
     r = min(r, n - r)
     numer = reduce(mul, range(n, n - r, -1), 1)
     denom = reduce(mul, range(1, r + 1), 1)
