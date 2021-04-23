848bb39e8e7db769d1e7cb37f2eec01ac2bee1ab
cozy_sauna <koji@cozy-sauna.local>
2021-04-23 15:47:46 +0900

    modified dijksta and add dinic

--- dijkstra.py
+++ dijkstra.py
@@ -1,23 +1,21 @@
-from heapq import heappush, heappop
-
 # node, edge, start
 v, e, r = map(int, input().split())
 node = [[] for _ in range(v+1)]
-for i in range(e):
+for _ in [0] * e:
     s, t, d = map(int, input().split())
     node[s].append((t, d))
     
 INF = 10 ** 9
-def dijkstra(s, n): # start and number of nodes
-    dist = [INF] * n
+def dijkstra(s): # start and number of nodes
+    from heapq import heappush, heappop
+    dist, done = [INF] * (n+1), [0]*(n+1)
     hq = [(0, s)] # (cost, node)
     dist[s] = 0
-    checked = [False] * n 
     while hq:
         v = heappop(hq)[1] # get a node
-        checked[v] = True
+        done[v] = 1
         for to, cost in node[v]: 
-            if checked[to] == False and dist[v] + cost < dist[to]:
-                dist[to] = dist[v] + cost
+            if done[to] == 0 and dist[v]+cost < dist[to]:
+                dist[to] = dist[v]+cost
                 heappush(hq, (dist[to], to))
     return dist
