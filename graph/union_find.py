96aafd6f9903539ced89f2319f95520c8eb83a63
cozy_sauna <koji@cozy-sauna.local>
2021-05-13 16:41:19 +0900

    modified union_find.py

--- graph/union_find.py
+++ graph/union_find.py
@@ -1,50 +1,37 @@
 class UnionFind():
     def __init__(self, n):
         self.n = n
-        self.parents = [-1] * n
+        self.P = [-1] * n # parents
 
     def find(self, x):
-        if self.parents[x] < 0:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
+        if self.P[x] < 0: return x
+        self.P[x] = self.find(self.P[x])
+        return self.P[x]
 
     def union(self, x, y):
         x = self.find(x)
         y = self.find(y)
+        if x == y: return
+        if self.P[x] > self.P[y]: x, y = y, x
+        self.P[x] += self.P[y]
+        self.P[y] = x
 
-        if x == y:
-            return
+    def size(self, x): return -self.P[self.find(x)]
 
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
-
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
-
-    def size(self, x):
-        return -self.parents[self.find(x)]
-
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
+    def same(self, x, y): return self.find(x) == self.find(y)
 
     def members(self, x):
         root = self.find(x)
         return [i for i in range(self.n) if self.find(i) == root]
 
-    def roots(self):
-        return [i for i, x in enumerate(self.parents) if x < 0]
+    def roots(self): return [i for i, x in enumerate(self.P) if x < 0]
 
-    def group_count(self):
-        return len(self.roots())
+    def group_count(self): return len(self.roots())
 
     def all_group_members(self):
         from collections import defaultdict
-        group_members = defaultdict(list)
-        for member in range(self.n):
-            group_members[self.find(member)].append(member)
-        return group_members
+        G = defaultdict(list)
+        for member in range(self.n): G[self.find(member)].append(member)
+        return G
 
-    def __str__(self):
-        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+    def __str__(self): return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
