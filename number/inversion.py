399318b4922169e88e159fb788f5fa078bf074a6
cozy_sauna <koji@cozy-sauna.local>
2021-05-13 14:08:41 +0900

    modified inversion

--- number/inversion.py
+++ number/inversion.py
@@ -1,8 +1,9 @@
 class Inv():
     def __init__(self, array):
         self.arr = array
-        self.l = len(array)+1
+        self.l = max(len(array), max(array))+1
         self.bit = [0]*(self.l+1)
+        self.cnt = dict()
 
     def add(self, x):
         x += 1
@@ -12,7 +13,8 @@ class Inv():
             
     def accum(self, x):
         x += 1
-        ret =0
+        s = x
+        ret = 0
         while x:
             ret += self.bit[x]
             x -= x & -x
@@ -22,9 +24,10 @@ class Inv():
         ret = 0
         for a in self.arr[::-1]:
             ret += self.accum(a)
+            if a in self.cnt: 
+                ret -= self.cnt[a]
+                self.cnt[a] += 1
+            else:
+                self.cnt[a] = 1
             self.add(a)
-
         return ret
-
-I = Inv(A) # [4, 3, 2, 1] ans: 6
-print(I.find())
