1e9645e4dc44426d044e04768396b7a37465b056
cozy_sauna <koji@cozy-sauna.local>
2021-05-20 16:37:49 +0900

    modified nCr_inverse.py

--- number/nCr_inverse.py
+++ number/nCr_inverse.py
@@ -1,16 +1,12 @@
-def nCr(n, r, p):
-    if (r < 0) or (n < r):
-        return 0
-    r = min(r, n - r)
-    return fact[n] * factinv[r] * factinv[n-r] % p
-
-M = 10 ** 9 + 7
-fact = [1, 1]  # fact[n] = (n! mod M)
-factinv = [1, 1]  # factinv[n] = ((n!)^(-1) mod M)
-inv = [0, 1]  # factinv 計算用
- 
+MOD = 10**9+7
+F, FI,INV = [1, 1], [1, 1], [0, 1]  
+n = 10**6
 for i in range(2, n + 1):
-    fact.append((fact[-1] * i) % M)
-    inv.append((-inv[M % i] * (M // i)) % M)
-    factinv.append((factinv[-1] * inv[-1]) % M)
+    F.append((F[-1] * i) % MOD)
+    INV.append((-INV[MOD % i] * (MOD // i)) % MOD)
+    FI.append((FI[-1] * INV[-1]) % MOD)
 
+def nCr(n, r, MOD):
+    if r < 0 or n < r: return 0
+    r = min(r, n - r)
+    return F[n] * FI[r] * FI[n-r] % MOD
