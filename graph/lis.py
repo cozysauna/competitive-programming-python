d89adf848669c76b5f9d0a9812722dafd43f5e8b
cozy_sauna <koji.1127.403@outlook.jp>
2021-09-01 10:37:58 +0900

    modified lis

--- graph/lis.py
+++ graph/lis.py
@@ -1,26 +1,10 @@
-class LIS():
-    def __init__(self, arr):
-        self.arr = arr
-        self.lis = self.find_lis(arr)
+def lis(arr):
+    import bisect
+    ans = [arr[0]]
+    for i in range(len(arr)):
+        if arr[i] > ans[-1]:
+            ans.append(arr[i])
+        else:
+            ans[bisect.bisect_left(ans, arr[i])] = arr[i]
     
-    def find_lis(self, arr):
-        def bis(idx, a, DP):
-            bot = -1
-            top = idx
-            while top - bot > 1:
-                mid = (top+bot) // 2
-                if DP[mid] < a: bot = mid
-                else: top = mid
-            return bot+1
-
-        INF = 10 ** 10
-        DP = [INF]*n
-        lis = 0
-        for i, a in enumerate(arr):
-            if not i: 
-                DP[i] = a
-                continue
-            cnt = bis(i, a, DP)
-            DP[cnt] = a
-            lis = max(lis, cnt+1)
-        return lis
+    return len(ans)
