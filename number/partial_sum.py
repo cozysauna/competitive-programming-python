8914cc1001b7b9b39ec0b07eb69c0e6c9a335f51
cozy_sauna <koji.1127.403@outlook.jp>
2021-10-10 12:03:08 +0900

    add partial_sum_judge

--- number/partial_sum.py
+++ number/partial_sum.py
@@ -8,3 +8,13 @@ def partial_sum_cases(arr, K, MOD=998244353):
                 dp[j+a] += dp[j]
                 dp[j+a] %= MOD
     return dp[K]
+
+
+def partial_sum_judge(arr, K):
+    if K < 0: return 0
+    dp = [0] * (K+1)
+    dp[0] = 1
+    for a in arr:
+        for j in range(K, -1, -1):
+            if j+a <= K: dp[j+a] = max(dp[j+a], dp[j])
+    return dp[K]
