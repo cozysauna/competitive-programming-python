18ac2265badd7cf0d29fd8550d8d8c8824f5040f
cozy_sauna <koji.1127.403@outlook.jp>
2022-03-27 01:37:40 +0900

    modified bit.py

--- graph/bit.py
+++ graph/bit.py
@@ -3,6 +3,7 @@ class BIT():
         self.size = N
         self.tree = [0] * (N + 1)
  
+    # A[i] += x
     def add(self, i, x):
         while i <= self.size:
             self.tree[i] += x
@@ -25,7 +26,7 @@ class BIT():
     def get(self, i):
         return self.sum(i) - self.sum(i - 1)
 
-    # max i (i <= x and 0 < A[i])
+    # max_i (i <= x and 0 < A[i])
     def less_than_x(self, x):
         base = self.sum(x)
         if base == 0: return None 
@@ -35,5 +36,16 @@ class BIT():
             if self.sum(mid) == base: top = mid 
             else: bot = mid  
         return top
+
+    # min_i (i >= x and 0 < A[i])
+    def more_than_x(self, x):
+        base = self.sum(x - 1)
+        if self.get_all_sum() - base == 0: return None 
+        bot, top = x - 1, self.size
+        while top - bot > 1:
+            mid = top + bot >> 1
+            if self.sum(mid) - base == 0: bot = mid 
+            else: top = mid 
+        return top 
 
     def get_all_sum(self): return self.sum(self.size)
