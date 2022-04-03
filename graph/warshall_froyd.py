5c48a732cc53ed40b3f8b358d1ea87a933279840
cozy_sauna <koji.1127.403@outlook.jp>
2022-04-03 15:20:27 +0900

    modified warshall_froyd.py

--- graph/warshall_froyd.py
+++ graph/warshall_froyd.py
@@ -1,8 +1,25 @@
-# cost: 2d-array, v: the number of nodes
-# if cost[i][i] is less than 0, this graph has a negative cycle
-def warshall_floyd(cost, v):
-    for k in range(v):
-        for i in range(v):
-            for j in range(v):
-                cost[i][j] = min(cost[i][j], cost[i][k]+cost[k][j])
-    return cost
+class WarshallFloyd:
+    def __init__(self, N, cost): # (a, b, c) dist[a][b] = c
+        self.dist = [[1 << 30] * N for _ in range(N) ]
+        for i in range(N): self.dist[i][i] = 0
+        for a, b, c in cost:
+            self.dist[a][b] = c 
+            self.dist[b][a] = c
+
+        self.prev = [[i] * N for i in range(N)]
+        for i in range(N):
+            for j in range(N):
+                for k in range(N):
+                    if self.dist[i][j] > self.dist[i][k] + self.dist[k][j]:
+                        self.dist[i][j] = self.dist[i][k] + self.dist[k][j]
+                        self.prev[i][j] = self.prev[k][j]
+                
+    def get(self, a, b): return self.dist[a][b]
+
+    def way(self, start, goal):
+        cur = goal
+        ret = [goal]
+        while cur != start:
+            cur = self.prev[start][cur]
+            ret.append(cur)
+        return ret[::-1]
