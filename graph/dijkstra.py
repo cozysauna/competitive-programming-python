855ae5cb95baabfc3cf8e86c25983eeea0ab61be
cozy_sauna <koji.1127.403@outlook.jp>
2022-04-03 16:50:28 +0900

    modified dijkstra.py

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -1,21 +1,18 @@
-# node, edge, start
-v, e, r = map(int, input().split())
-node = [[] for _ in range(v+1)]
-for _ in [0] * e:
-    s, t, d = map(int, input().split())
-    node[s].append((t, d))
-    
-INF = 10 ** 9
-def dijkstra(s): # start and number of nodes
-    from heapq import heappush, heappop
-    dist, done = [INF] * (n+1), [0]*(n+1)
-    hq = [(0, s)] # (cost, node)
-    dist[s] = 0
-    while hq:
-        v = heappop(hq)[1] # get a node
-        done[v] = 1
-        for to, cost in node[v]: 
-            if done[to] == 0 and dist[v]+cost < dist[to]:
-                dist[to] = dist[v]+cost
-                heappush(hq, (dist[to], to))
-    return dist
+from heapq import heappush, heappop
+class Dikstra:
+    def __init__(self, N, node):
+        self.node = node 
+        self.N = N 
+
+    def get(self, start, goal):
+        dist, done = [10 ** 18] * self.N, [0] * self.N
+        hq = [(0, start)] # (cost, node)
+        dist[start] = 0
+        while hq:
+            c, v = heappop(hq)
+            done[v] = 1
+            for to, cost in self.node[v]: 
+                if done[to] == 0 and dist[v] + cost < dist[to]:
+                    dist[to] = dist[v]+cost
+                    heappush(hq, (dist[to], to))
+        return dist[goal]
