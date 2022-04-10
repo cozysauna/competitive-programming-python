4913ffaa864664cae6cb619cc50726da1daf06bc
cozy_sauna <koji.1127.403@outlook.jp>
2022-04-11 01:46:03 +0900

    modified ext_gcd.py

--- number/ext_gcd.py
+++ number/ext_gcd.py
@@ -1,4 +1,5 @@
-def extgcd(a, b): # ax + by = d (x, y, d)
+ # ax + by = d return (x, y, d); d = gcd(a, b)
+def extgcd(a, b):
     if not b: return (1, 0, a)
     x, y, d = extgcd(b, a % b)
     return y, x - a // b * y, d
