4e00a3c877f690a8d32c4f30d724477a6d247ba0
cozy_sauna <koji.1127.403@outlook.jp>
2022-04-15 23:19:43 +0900

    modified prim.py

--- graph/prim.py
+++ graph/prim.py
@@ -1,16 +1,17 @@
-def prim(node):
-    from heapq import heappush, heappop
+def prim(node, N):
     ans = 0
     cnt = 0
-    q = [(0, 0)] #cost, start_node
-    done = [0]*n
-    while q:
-        c, v = heappop(q)
+    que = [(0, 0)]
+    done = [0] * N
+    while que:
+        c, v = heappop(que)
         if done[v]: continue
         done[v] = 1
         cnt += 1
         ans += c
-        if cnt == n: break
-        for cost, nx in node[v]: heappush(q,(cost, nx))
+        if cnt == N: break
+        for nx, cost in node[v]: 
+            if done[nx]: continue
+            heappush(que, (cost, nx))
 
     return ans
