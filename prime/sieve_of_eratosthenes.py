ab12aa6d5044cea1c520e843b39a8fca728277ad
cozy_sauna <koji.1127.403@outlook.jp>
2022-05-09 11:43:27 +0900

    modified sieve_of_eratostheneses.py

--- prime/sieve_of_eratosthenes.py
+++ prime/sieve_of_eratosthenes.py
@@ -1,14 +1,14 @@
 # primes less than N
-# primes(10) = 2, 3, 5, 7
- # O(NloglogN)
+# sieve(10) = 2, 3, 5, 7
+# O(NloglogN)
 
-def primes(n): 
+def sieve(N): 
     P = []
-    check = [1]*(n+1)
+    check = [1] * (N + 1)
     check[1] = 0
-    for p in range(2, n+1):
+    for p in range(2, N + 1):
         if not check[p]: continue
         P.append(p)
-        for i in range(p*p, n+1, p): check[i] = 0
+        for i in range(p * p, N + 1, p): check[i] = 0
     
     return P 
