22a5f5773b17b8eec4d15bc316edbaeb34e31aaa
cozy_sauna <koji.1127.403@outlook.jp>
2022-05-26 16:08:45 +0900

    modified lis.py

--- graph/lis.py
+++ graph/lis.py
@@ -1,10 +1,18 @@
-def lis(arr):
-    import bisect
-    ans = [arr[0]]
-    for i in range(len(arr)):
-        if arr[i] > ans[-1]:
-            ans.append(arr[i])
-        else:
-            ans[bisect.bisect_left(ans, arr[i])] = arr[i]
+from bisect import bisect_left
+
+def lis(A):
+    table = [A[0]]
+    for a in A:
+        if a > table[-1]: table.append(a)
+        else: table[bisect_left(table, a)] = a
     
-    return len(ans)
+    return len(table)
+
+def lis2(A): # slower lis
+    INF = 1 << 30
+    table = [INF] * len(A)
+    for a in A: 
+        table[bisect_left(table, a)] = a
+
+    ans = bisect_left(table, INF)
+    return ans 
