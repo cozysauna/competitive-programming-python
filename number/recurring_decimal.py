6815dc373d27bad7e0abd247bebab0c769916ae6
cozy_sauna <koji.1127.403@outlook.jp>
2022-06-10 15:05:46 +0900

    modified recurring_decimal.py

--- number/recurring_decimal.py
+++ number/recurring_decimal.py
@@ -1,9 +1,31 @@
-def rec_dec(x): # (length, recurring decimal)
-    r, ix, rs, s = 1, 0, [0]*x, ''
-    while 1:
-        q, r = r//x, r%x  # x = q*y + r
-        ix, s = ix+1, s+str(q)
-        if not r: return (0, s)
-        if rs[r]: return (ix-rs[r], s[rs[r]:])
-        rs[r] = ix
-        r *= 10 
+def gcd(x, y):
+    while y: x, y = y, x % y 
+    return x 
+
+def is_terminate_decimal(numerator, denominator):
+    div = gcd(numerator, denominator)
+    denominator //= div 
+    while denominator % 2 == 0: denominator //= 2 
+    while denominator % 5 == 0: denominator //= 5 
+    return denominator == 1
+
+def recurring_decimal(numerator, denominator):
+    # not terminate decimal
+    assert not is_terminate_decimal(numerator, denominator)
+
+    integer, numerator = divmod(numerator, denominator)
+    decimal = []
+    idx = 0
+    checked = {}
+    quotient, remainder = divmod(numerator * 10, denominator)
+    while (quotient, remainder) not in checked:
+        checked[(quotient, remainder)] = idx 
+        decimal.append(quotient)
+        numerator = remainder
+        idx += 1
+        quotient, remainder = divmod(numerator * 10, denominator)
+
+    repeat = decimal[checked[(quotient, remainder)]:]
+    ans = str(integer) + '.' + ''.join(map(str, decimal))
+    print(ans)
+    print(f"Repeat:{repeat}")
