62d5eeef2b96adcecb50b0ffd09df324547bff8d
cozy_sauna <koji.1127.403@outlook.jp>
2022-06-10 15:43:20 +0900

    modified mat_pow.py

--- matrix/mat_pow.py
+++ matrix/mat_pow.py
@@ -1,5 +1,8 @@
 def mat_pow(A, n):
-    if not n: return [[1, 0], [0, 1]]
+    if not n: 
+        identity_matrix = [[1 if i == j else 0 for i in range(len(A))] for j in range(len(A))]
+        return identity_matrix
+
     m = mat_pow(A, n//2)
     if n % 2:
         return mat_mul(A, mat_mul(m, m))
