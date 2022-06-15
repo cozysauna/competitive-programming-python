0ecbc8a017932a271bd9dce5c7dede2c831bbb72
cozy_sauna <koji.1127.403@outlook.jp>
2022-06-15 12:28:40 +0900

    modified bit.py

--- graph/bit.py
+++ graph/bit.py
@@ -2,17 +2,17 @@ class BIT(): # 1-indexed
     def __init__(self, N):
         self.size = N
         self.tree = [0] * (N + 1)
+        self.arr = [0] * (N + 1)
  
     # A[i] += x
     def add(self, i, x):
+        self.arr[i] += x
         while i <= self.size:
             self.tree[i] += x
             i += i & -i
 
-    # A[i] = x #TOO SLOW(Use add if possible)
-    def update(self, i, x):
-        old = self.get(i)
-        self.add(i, x - old)
+    # A[i] = x 
+    def update(self, i, x):self.add(i, x - self.arr[i])
 
     # A[1] + A[2] + ... + A[i]
     def sum(self, i):
@@ -23,31 +23,30 @@ class BIT(): # 1-indexed
         return ret
 
     # A[i]
-    def get(self, i):
-        return self.sum(i) - self.sum(i - 1)
+    def get(self, i): return self.arr[i]
 
-    # max_i (i <= x and 0 < A[i])
+    # max_i (i <= x and A[i] > 0)
     def less_than_x(self, x):
         base = self.sum(x)
         if base == 0: return None 
-        bot, top = 0, x 
-        while top - bot > 1:
-            mid = top + bot >> 1
-            if self.sum(mid) == base: top = mid 
-            else: bot = mid  
-        return top
+        ng, ok = 0, x
+        while ok - ng > 1:
+            mid = ok + ng >> 1
+            if self.sum(mid) == base: ok = mid 
+            else: ng = mid 
+        return ok 
 
-    # min_i (i >= x and 0 < A[i])
+    # min_i (i >= x and A[i] > 0)
     def more_than_x(self, x):
         base = self.sum(x - 1)
         if self.get_all_sum() - base == 0: return None 
-        bot, top = x - 1, self.size
-        while top - bot > 1:
-            mid = top + bot >> 1
-            if self.sum(mid) - base == 0: bot = mid 
-            else: top = mid 
-        return top 
+        ng, ok = x - 1, self.size
+        while ok - ng > 1:
+            mid = ng + ok >> 1
+            if self.sum(mid) - base == 0: ng = mid 
+            else: ok = mid 
+        return ok
 
     def get_all_sum(self): return self.sum(self.size)
 
-    def display_all(self): print(*[self.get(i) for i in range(1, self.size + 1)])
+    def display_all(self): print(*self.arr[1:])
