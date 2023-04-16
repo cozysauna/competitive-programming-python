94dc09635df51b8fca22fafe8cdc61e76cea020e
cozy_sauna <koji.1127.403@outlook.jp>
2023-04-16 17:23:01 +0900

    add get_size method on compress.py

--- number/compress.py
+++ number/compress.py
@@ -5,12 +5,16 @@ class Compress:
         for i, e in enumerate(sorted(set(A)), start):
             self.compress[e] = i
             self.original[i] = e 
+        self.N = len(self.compress)
 
-    # 座圧後の要素を取得する
+    # 座圧後の要素を取得する object[x] -> compress[x]
     def __getitem__(self, x):
         return self.compress[x]
 
     # 座圧前の要素を取得する
     def rev(self, x):
         return self.original[x]
-        
+
+    # 座圧後のサイズを取得する
+    def get_size(self):
+        return self.N
