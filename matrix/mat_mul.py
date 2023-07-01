fc56c82620b9d1c0ba61309de6d42485127d20ed
cozy_sauna <koji.1127.403@outlook.jp>
2023-07-01 18:54:51 +0900

    modified mat_mul.py

--- matrix/mat_mul.py
+++ matrix/mat_mul.py
@@ -2,12 +2,12 @@ def mat_mul(A, B):
     y1, x1 = len(A), len(A[0])
     y2, x2 = len(B), len(B[0])
     assert x1 == y2 
-    mat = [[0] * x2 for _ in range(y1)]
-    for i in range(y1):
-        for j in range(x2):
-            for k in range(x1):
-                # mat[i][j] ^= A[i][k] & B[k][j]
-                mat[i][j] += A[i][k] * B[k][j]
+    mat = [[0] * x2 for _ in [None] * y1]
+
+    for i, mati in enumerate(mat):
+        for k, aik in enumerate(A[i]):
+            for j, bkj in enumerate(B[k]):
+                # mati[j] ^= aik & bkj
+                mati[j] += aik * bkj
 
     return mat 
-    
