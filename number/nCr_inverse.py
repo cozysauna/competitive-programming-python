d091e3285907537b877d5607ec3960ea2443f28e
cozy_sauna <koji.1127.403@outlook.jp>
2023-07-29 11:45:54 +0900

    modified nCr_inverse

--- number/nCr_inverse.py
+++ number/nCr_inverse.py
@@ -1,16 +1,12 @@
-class nCrInverse:
-    def __init__(self, N, MOD):
-        self.N = N 
-        self.MOD = MOD 
-        self.F = [1, 1]
-        self.FI = [1, 1]
-        self.INV = [0, 1]
-        for i in range(2, self.N + 1):
-            self.F.append((self.F[-1] * i) % MOD)
-            self.INV.append((-self.INV[MOD % i] * (MOD // i)) % MOD)
-            self.FI.append((self.FI[-1] * self.INV[-1]) % MOD)
+F = [1, 1]
+FI = [1, 1]
+INV = [0, 1]
+for i in range(2, N + 1):
+    F.append((F[-1] * i) % MOD)
+    INV.append((-INV[MOD % i] * (MOD // i)) % MOD)
+    FI.append((FI[-1] * INV[-1]) % MOD)
 
-    def get(self, n, r):
-        if r < 0 or n < r: return 0
-        r = min(r, n - r)
-        return self.F[n] * self.FI[r] * self.FI[n - r] % self.MOD
+def nCr(n, r):
+    if r < 0 or n < r: return 0
+    r = min(r, n - r)
+    return F[n] * FI[r] * FI[n - r] % MOD
