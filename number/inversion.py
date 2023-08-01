f499551450232b1e627b006118c55a0d7be746a7
cozy_sauna <koji.1127.403@outlook.jp>
2023-08-01 22:07:09 +0900

    modified inversion.py

--- number/inversion.py
+++ number/inversion.py
@@ -1,26 +1,23 @@
-class Inv:
-    def __init__(self, N, A):
-        self.size = N
-        self.tree = [0] * (N + 1)
-        if 0 in A: 
-            for i in range(N): A[i] += 1
-        self.A = A
+# 1-indexed A[i] > A[j] (i, j)
+def inversion(A):
 
-    def add(self, i, x):
-        while i <= self.size:
-            self.tree[i] += x
-            i += i & -i
+    N = len(A)
+    data = [0] * (N + 1)
 
-    def sum(self, i):
+    def _sum(i):
         ret = 0
         while i > 0:
-            ret += self.tree[i]
+            ret += data[i]
             i -= i & -i
         return ret
 
-    def get(self):
-        cnt = 0
-        for i, a in enumerate(self.A):
-            self.add(a, 1)
-            cnt += i + 1 - self.sum(a)
-        return cnt
+    def _add(i, x):
+        while i <= N:
+            data[i] += x
+            i += i & -i
+
+    inv = 0
+    for a in A[::-1]:
+        inv += _sum(a - 1)
+        _add(a, 1)
+    return inv
